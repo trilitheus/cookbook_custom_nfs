28f1fac60cc84b00ac50424d54226a3ddda24cb2
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-06 14:45:12 +0100

    Add initial spec and start default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,1 @@
-#
-# Cookbook Name:: custom_nfs
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+include_recipe 'nfs::default'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,13 +8,13 @@ require 'spec_helper'
 
 describe 'custom_nfs::default' do
   context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
+    cached(:chef_run) do
       runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'includes the nfs::default recipe' do
+      expect(chef_run).to include_recipe('nfs::default')
     end
   end
 end
