90e95dff838803ad8387f1318cd77096c53474a1
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-10 15:00:25 +0100

    Comply with rubocop

--- test/integration/server/nfs.rb
+++ test/integration/server/nfs.rb
@@ -4,17 +4,17 @@
   end
 end
 
-describe iptables(table:'filter', chain:'IN_public_allow') do
+describe iptables(table: 'filter', chain: 'IN_public_allow') do
   it { should have_rule('-A IN_public_allow -p tcp -m tcp --dport 2049 -m conntrack --ctstate NEW -j ACCEPT') }
   it { should have_rule('-A IN_public_allow -p tcp -m tcp --dport 20048 -m conntrack --ctstate NEW -j ACCEPT') }
   it { should have_rule('-A IN_public_allow -p udp -m udp --dport 20048 -m conntrack --ctstate NEW -j ACCEPT') }
 end
 
 describe file('/etc/exports') do
-  its('content') { should match %r(data 33.33.33.1/24\(rw,sync,no_root_squash\)) }
+  its('content') { should match %r{data 33.33.33.1/24\(rw,sync,no_root_squash\)} }
 end
 
 check_exports = command('exportfs -v')
 describe check_exports do
-  its('stdout') { should match %r(/data\s+33.33.33.1/24\(rw,wdelay,no_root_squash,no_subtree_check,sec=sys,rw,secure,no_root_squash,no_all_squash\)) }
+  its('stdout') { should match %r{/data\s+33.33.33.1/24\(rw,wdelay,no_root_squash,no_subtree_check,sec=sys,rw,secure,no_root_squash,no_all_squash\)} }
 end
