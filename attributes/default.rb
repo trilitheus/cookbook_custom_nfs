a05175fedb4b622f3d99d60977f534ace043b678
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-10 11:12:52 +0100

    Update attributes for nfsv4

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['custom_nfs']['share_to'] = '33.33.33.1/24'
 default['custom_nfs']['mount_from'] = '33.33.33.2'
 default['custom_nfs']['writable'] = true
 default['custom_nfs']['sync'] = true
-default['custom_nfs']['share_options'] = 'data'
-default['custom_nfs']['mount_options'] = 'default,noatime,nodiratime,vers=3'
+default['custom_nfs']['share_options'] = ['no_root_squash']
+default['custom_nfs']['mount_options'] = ['defaults','noatime','nodiratime','vers=4']
