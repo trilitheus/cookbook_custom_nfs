e02725035b48452da3efb33bd1b72fab5a216141
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-10-10 12:27:22 +0100

    Comply with Rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,4 @@ default['custom_nfs']['mount_from'] = '33.33.33.2'
 default['custom_nfs']['writable'] = true
 default['custom_nfs']['sync'] = true
 default['custom_nfs']['share_options'] = ['no_root_squash']
-default['custom_nfs']['mount_options'] = ['defaults','noatime','nodiratime','vers=4']
+default['custom_nfs']['mount_options'] = ['defaults', 'noatime', 'nodiratime', 'vers=4']
