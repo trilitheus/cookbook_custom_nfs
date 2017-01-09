df775f72e1aafcce5e5cb09422b09045f9e8dedc
Roy Crombleholme <roy.crombleholme@pam-it.com>
2017-01-09 09:30:54 +0000

    Update lvm gem attributes

--- attributes/lvm.rb
+++ attributes/lvm.rb
@@ -4,3 +4,7 @@ default['custom_nfs']['lv']['name'] = 'datavol1'
 default['custom_nfs']['lv']['size'] = '100%VG'
 default['custom_nfs']['lv']['fs_type'] = 'ext4'
 default['custom_nfs']['lv']['options'] = 'noatime,nodiratime'
+
+default['lvm']['chef-ruby-lvm']['version'] = '0.2.2'
+default['lvm']['chef-ruby-lvm-attrib']['version'] = '0.0.28'
+default['lvm']['cleanup-old-gems']['version'] = true
